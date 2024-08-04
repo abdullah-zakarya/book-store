aea6d95f054a43da0337d0952776c8323b21333c
abdullah zakaria <mohtarfana@gmail.com>
2024-08-04 13:34:15 +0300

    basic user methodes from admin interface

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 1) Import mudels -
 const express = require("express");
 const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // 2) initalize the app
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 // 4) Secury
 // 5) Routes
 app.use("/api/v1/book", bookRoutes);
+app.use("/api/v1/user", userRoutes);
 // 6) Path error handle
 // 7) export the mudel
 module.exports = app;
