82d30efd7ec9b31447b2867d9e1d65ff05cedf58
abdullah zakaria <mohtarfana@gmail.com>
2024-08-06 19:17:23 +0300

    authorization metodes complate�

---
+++ routes/authRoutes.js
@@ -0,0 +1,7 @@
+const authController = require('./../controller/authController');
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
